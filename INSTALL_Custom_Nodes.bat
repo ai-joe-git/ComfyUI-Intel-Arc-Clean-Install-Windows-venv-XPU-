b72f61042c360acecb2cd41c58138c1bff696ad4
A.I Joe <88169436+ai-joe-git@users.noreply.github.com>
2026-02-10 22:41:08 +0100

    Update INSTALL_Custom_Nodes.bat

--- INSTALL_Custom_Nodes.bat
+++ INSTALL_Custom_Nodes.bat
@@ -11,14 +11,15 @@ echo   - ComfyUI-GGUF (quantized models)
 echo   - ComfyUI-VideoHelperSuite (video tools)
 echo   - ComfyUI-Impact-Pack (utilities)
 echo   - rgthree-comfy (workflow tools)
+echo   - gguf (GGUF model support)
 echo.
 pause
 
-cd /d C:\ComfyUI\custom_nodes
-call ..\comfyui_venv\Scripts\activate.bat
+cd /d C:\\ComfyUI\\custom_nodes
+call ..\\comfyui_venv\\Scripts\\activate.bat
 
 echo.
-echo [1/5] Installing ComfyUI-Manager...
+echo [1/6] Installing ComfyUI-Manager...
 if not exist "ComfyUI-Manager" (
     git clone https://github.com/ltdrdata/ComfyUI-Manager
     cd ComfyUI-Manager
@@ -33,7 +34,7 @@ if not exist "ComfyUI-Manager" (
 )
 
 echo.
-echo [2/5] Installing ComfyUI-GGUF...
+echo [2/6] Installing ComfyUI-GGUF...
 if not exist "ComfyUI-GGUF" (
     git clone https://github.com/city96/ComfyUI-GGUF
 ) else (
@@ -44,7 +45,7 @@ if not exist "ComfyUI-GGUF" (
 )
 
 echo.
-echo [3/5] Installing ComfyUI-VideoHelperSuite...
+echo [3/6] Installing ComfyUI-VideoHelperSuite...
 if not exist "ComfyUI-VideoHelperSuite" (
     git clone https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite
     cd ComfyUI-VideoHelperSuite
@@ -59,7 +60,7 @@ if not exist "ComfyUI-VideoHelperSuite" (
 )
 
 echo.
-echo [4/5] Installing ComfyUI-Impact-Pack...
+echo [4/6] Installing ComfyUI-Impact-Pack...
 if not exist "ComfyUI-Impact-Pack" (
     git clone https://github.com/ltdrdata/ComfyUI-Impact-Pack
     cd ComfyUI-Impact-Pack
@@ -74,13 +75,28 @@ if not exist "ComfyUI-Impact-Pack" (
 )
 
 echo.
-echo [5/5] Installing rgthree-comfy...
+echo [5/6] Installing rgthree-comfy...
 if not exist "rgthree-comfy" (
     git clone https://github.com/rgthree/rgthree-comfy
 ) else (
     echo Already installed, updating...
     cd rgthree-comfy
     git pull
+    cd ..
+)
+
+echo.
+echo [6/6] Installing gguf...
+if not exist "gguf" (
+    git clone https://github.com/calcuis/gguf
+    cd gguf
+    if exist requirements.txt pip install -r requirements.txt
+    cd ..
+) else (
+    echo Already installed, updating...
+    cd gguf
+    git pull
+    if exist requirements.txt pip install -r requirements.txt
     cd ..
 )
 
